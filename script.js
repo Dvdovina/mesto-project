40c3e59ba076385997a4e1be3cccdfb00d4bbf25
Daria Vdovina <vdovinadn@gmail.com>
2022-12-18 05:15:37 +0300

    Модальное окно для создания карточки

--- script.js
+++ script.js
@@ -2,23 +2,28 @@
 const popupEditBtn = document.querySelector('.profile__edit-button');
 const profileName = document.querySelector('.profile__name');
 const profileInfo = document.querySelector('.profile__description');
-const popup = document.querySelector('.popup');
-const popupCloseBtn = popup.querySelector('.popup__close-button')
-const popupSaveBtn = popup.querySelector('.popup__submit-button')
-const popupForm = popup.querySelector('.popup__form');
+const popupProfile = document.querySelector('.popup__profile');
+const popupCloseBtn = popupProfile.querySelector('.popup__close-button');
+const popupSaveBtn = popupProfile.querySelector('.popup__submit-button');
+const popupForm = popupProfile.querySelector('.popup__form');
 const nameInput = popupForm.querySelector('#Name');
 const jobInput = popupForm.querySelector('#About');
+const popupCard = document.querySelector('.popup__card');
+const popupCardOpenBtn = document.querySelector('.profile__add-button');
+const popupCardCloseBtn = popupCard.querySelector('.popup__close-button');
+const popupCardCreateBtn = popupCard.querySelector('.popup__submit-button');
+const cardTitle = popupCard.querySelector('#title');
+const cardLink = popupCard.querySelector('#link');
 
-
-// Реализация открытия и закрытия модального окна
+/////////// Реализация открытия и закрытия модального окна
 
 // Универсальная функция для открытия popup
-function popupOpen(popup){
+function openPopup(popup) {
     popup.classList.add('popup_opened');
 };
 
 // Универсальная функция для закрытия popup 
-function popupClose(popup){
+function closePopup(popup) {
     popup.classList.remove('popup_opened');
 };
 
@@ -26,17 +31,17 @@ function popupClose(popup){
 popupEditBtn.addEventListener('click', function()  {
     nameInput.value = profileName.textContent;
     jobInput.value = profileInfo.textContent;
-    popupOpen(popup);
+    openPopup(popupProfile);
 });
 
 //Закрытие попапа profile
 popupCloseBtn.addEventListener('click', function()  {
-    popupClose(popup);
+    closePopup(popupProfile);
 });
 
 //Закрытие и сохранение данных попапа через Save Button
 popupSaveBtn.addEventListener ('click', function()  {
-    popupClose(popup);
+    closePopup(popupProfile);
 });
 
 //Обработчик отправки формы
@@ -49,3 +54,29 @@ function submitProfileForm(evt) {
 
 //Прикрепление обработчика к форме
 popupForm.addEventListener('submit', submitProfileForm);
+
+
+
+////////////Добавление карточки пользователем
+
+
+
+
+
+
+
+///////////Реализация открытия модального окна для создания карточки
+popupCardOpenBtn.addEventListener('click', function()  {
+    openPopup(popupCard);
+});
+
+//Закрытие карточки
+popupCardCloseBtn.addEventListener('click', function()  {
+    closePopup(popupCard);
+});
+
+//Функционал кнопки создания карточки
+popupCardCreateBtn.addEventListener('click', function()  {
+    closePopup(popupCard);
+});
+
