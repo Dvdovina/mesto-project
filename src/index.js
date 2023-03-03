7ee4f761cb112a8eb3e7f4c00a1b59f4ecbc1e93
Daria Vdovina <vdovinadn@gmail.com>
2023-03-03 21:48:53 +0300

    feature: form validation on js

--- src/index.js
+++ src/index.js
@@ -56,25 +56,25 @@ function closeOnEsc(evt) {
 //Закрытие popupProfile по клику на оверлей
 popupProfile.addEventListener("click", (evt) => {
     if (evt.currentTarget === evt.target) {
-      closePopup(popupProfile)
+        closePopup(popupProfile)
     }
-  })
+})
 
 //Закрытие popupCard по клиу на оверлей
 
 popupCard.addEventListener("click", (evt) => {
     if (evt.currentTarget === evt.target) {
-      closePopup(popupCard)
+        closePopup(popupCard)
     }
-  })
+})
 
 //Закрытие popupImg по клиу на оверлей
 
 popupImg.addEventListener("click", (evt) => {
     if (evt.currentTarget === evt.target) {
-      closePopup(popupImg)
+        closePopup(popupImg)
     }
-  })
+})
 
 
 //////Попап редактирование профиля:
@@ -156,3 +156,94 @@ function submitCardForm(evt) {
 
 //Прикрепление обработчика к форме
 popupCardForm.addEventListener('submit', submitCardForm);
+
+
+//Валидация форм
+
+const settings = {
+    formSelector: '.popup__form',
+    inputSelector: '.popup__input',
+    submitButtonSelector: '.popup__submit-button',
+    inactiveButtonClass: 'popup__submit-button_disabled',
+    inputErrorClass: 'popup__input_type_error',
+    errorClass: 'popup__error_active'
+  };
+
+
+const showInputError = (formElement, inputElement, errorMessage, settings) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add(settings.inputErrorClass);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add(settings.errorClass);
+};
+
+
+const hideInputError = (formElement, inputElement, settings) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove(settings.inputErrorClass);
+    errorElement.classList.remove(settings.errorClass);
+    errorElement.textContent = '';
+};
+
+
+const isValid = (formElement, inputElement) => {
+    if (inputElement.validity.patternMismatch) {
+        inputElement.setCustomValidity(inputElement.dataset.errorMessage);
+  } else {
+    inputElement.setCustomValidity("");
+  }
+    if (!inputElement.validity.valid) {
+        showInputError(formElement, inputElement, inputElement.validationMessage, settings);
+    } else {
+        hideInputError(formElement, inputElement, settings);
+    }
+};
+
+
+const hasInvalidInput = (inputList) => {
+    return inputList.some((inputElement) => {
+        return !inputElement.validity.valid;
+    })
+};
+  
+
+const toggleButtonState = (inputList, buttonElement) => {
+    if (hasInvalidInput(inputList)) {
+        buttonElement.disabled = true;
+        buttonElement.classList.add(settings.inactiveButtonClass);
+    } else {
+        buttonElement.disabled = false;
+        buttonElement.classList.remove(settings.inactiveButtonClass);
+    }
+};
+
+const setEventListeners = (formElement) => {
+    const inputList = Array.from(formElement.querySelectorAll(settings.inputSelector));
+    const buttonElement = formElement.querySelector(settings.submitButtonSelector);
+    toggleButtonState(inputList, buttonElement);
+    inputList.forEach((inputElement) => {
+        inputElement.addEventListener('input', () => {
+            isValid(formElement, inputElement)
+            toggleButtonState(inputList, buttonElement);
+        });
+    });
+    formElement.addEventListener('reset', () => {
+        setTimeout(() => {
+          toggleButtonState(inputList, buttonElement, selectors), 0
+        });
+      });
+    };
+
+
+const enableValidation = () => {
+    const formList = Array.from(document.querySelectorAll(settings.formSelector));
+    formList.forEach((formElement) => {
+        formElement.addEventListener('submit', (evt) => {                       
+            evt.preventDefault();                                                                       
+          });
+        setEventListeners(formElement);
+    });
+};
+
+enableValidation();
+
