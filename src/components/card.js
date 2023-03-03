713a37c389b2fa91bb6d1e7e78577818cf3099c9
Daria Vdovina <vdovinadn@gmail.com>
2023-03-03 22:19:03 +0300

    refactor: assign js to different files

--- src/components/card.js
+++ src/components/card.js
@@ -24,3 +24,47 @@ const initialCards = [
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
     ];
+
+
+    ////////////Добавление карточки пользователем
+
+const cardTemplate = document.querySelector('#card-template').content;
+const cardsList = document.querySelector('.elements__grid');
+
+//Функция для создания и просмотра карточки пользователем (клонируем template, наполняем содержимым, удаление, like button)
+function createCard(titleValue, imgValue) {
+  const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+  cardElement.querySelector('.element__title').textContent = titleValue;
+  const cardImage = cardElement.querySelector('.element__image');
+  cardImage.src = imgValue;
+  cardImage.alt = titleValue;
+  cardImage.addEventListener('click', function () {
+      popupImg.querySelector('.popup__img-photo').src = imgValue;
+      popupImg.querySelector('.popup__img-photo').alt = titleValue;
+      popupImg.querySelector('.popup__img-caption').textContent = titleValue;
+      openPopup(popupImg);
+  });
+  cardElement.querySelector('.element__like-button').addEventListener('click', function (evt) {
+      evt.target.classList.toggle('element__like-button_active');
+  });
+  cardElement.querySelector('.element__delete-button').addEventListener('click', function () {
+      cardElement.remove();
+  });
+  return cardElement;
+};
+
+//Функция добавления новой карточки
+function addNewCard(card, container) {
+  container.prepend(card);
+};
+
+
+//Функция добавления 6 шаблонных карточек
+function loadCards(array) {
+  array.forEach(item => {
+      const element = createCard(item.name, item.link);
+      addNewCard(element, cardsList);
+  });
+};
+
+loadCards(initialCards)
